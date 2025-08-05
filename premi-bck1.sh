f48fdaddc62662b22bb19100c20098959c941572
gedozew <156336014+gedozew@users.noreply.github.com>
2025-08-05 13:41:53 +0700

    Update and rename premi.sh to premi-bck1.sh

--- premi-bck1.sh
+++ premi-bck1.sh
@@ -21,8 +21,8 @@ TIME=$(date '+%d %b %Y')
 ipsaya=$(wget -qO- ipinfo.io/ip)
 TIMES="10"
 CHATID="6617783693"
-KEY="6751589620:AAHwjP6dzZhuqeyUOdYFc6742Q1YUVF1EjM"
-URL="https://api.telegram.org/bot$KEY/sendMessage"
+##KEY="6751589620:AAHwjP6dzZhuqeyUOdYFc6742Q1YUVF1EjM"
+###URL="https://api.telegram.org/bot$KEY/sendMessage"
 # ===================
 clear
   # // Exporint IP AddressInformation
